29d2a83d65f93cb090e3820bed33d91dacacc2b8
Sidneyr <sidneyr@Sidneyrs-MacBook-Pro.local>
2016-12-23 11:15:40 -0600

    Can now add new users.

--- app/routes/new-user.js
+++ app/routes/new-user.js
@@ -3,13 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   firebaseApp : Ember.inject.service(),
   actions: {
-    addUser: function(email, password) {
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
-    }
+
+  addUser(email, pass) {
+    const auth = this.get('firebaseApp').auth();
+    auth.createUserWithEmailAndPassword(email, pass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
   }
+
+}
 });
