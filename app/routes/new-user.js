01da3c01edd3066e0ca2f5ba5ab8e9da149b18a5
Sidneyr <sidneyr@Sidneyrs-MBP.attlocal.net>
2016-12-22 22:23:02 -0600

    Must be authenticated to view any pages other than index OR login

--- app/routes/new-user.js
+++ app/routes/new-user.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  firebaseApp : Ember.inject.service(),
+  actions: {
+    addUser: function(email, password) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+    }
+  }
 });
