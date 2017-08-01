cdf2c28843d9eef375cc2672faafb43385367d7c
Sidneyr <20401907+sidneyrehg@users.noreply.github.com>
2017-08-01 18:15:31 -0500

    added search functionality for admin users

--- app/controllers/members.js
+++ app/controllers/members.js
@@ -1,9 +1,24 @@
 import Ember from 'ember';
+import computedFilterByQuery from 'ember-cli-filter-by-query';
 
 export default Ember.Controller.extend({
 
+  actions: {
+    resetPage: function() {
+      this.set('page', 1);
+    }
+  },
+
   sortProperties: ['title:asc'],
   sortedItems: Ember.computed.sort('model', 'sortProperties'),
+
+  query: null,
+  filteredContent: computedFilterByQuery(
+      'sortedItems',
+      ['title', 'email', 'description', 'stamps'],
+      'query',
+      { conjunction: 'and', sort: false}
+    ),
 
   totalCount: Ember.computed.alias('model.length'),
   completeCount: Ember.computed('model.@each.complete', function() {
