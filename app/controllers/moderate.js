f5a3311369dbb58d4655860c82df022459d53781
Sidneyr <20401907+sidneyrehg@users.noreply.github.com>
2018-05-11 15:52:03 -0500

    testing for Alec

--- app/controllers/moderate.js
+++ app/controllers/moderate.js
@@ -36,3 +36,6 @@ export default Ember.Controller.extend({
   }
 
 });
+
+<!—[if lte IE 8]>
+ <!--[if lte IE 8]> <div style="width: 100%; background: red; border: 1px black; padding-top: 10px; padding-bottom: 10px; color: #fff; text-align: center; font-weight: bold; font-size: 18px"> Sorry, you are using an unsupported browser. This page will not display correctly. <br /> <a href="http://www.whatbrowser.org" style="color: #fff;" target="_blank"> Please click here to upgrade to a newer browser. </a> </div> <![endif]--> <script src="https://embed-452835.secondstreetapp.com/Scripts/dist/embed.js" data-ss-embed="promotion" data-opguid="227a990b-94a1-453c-aebd-2a0ac05c116e" data-routing="hash">/**/</script>
